996b67263aab014b8c7d9f2e3512dc5d1aa9410a
zyzy <131111603+saffanazyan07@users.noreply.github.com>
2024-10-16 16:45:35 +0800

    Update register.cpp

--- src/ue/nas/mm/register.cpp
+++ src/ue/nas/mm/register.cpp
@@ -12,7 +12,7 @@
 #include <lib/nas/utils.hpp>
 #include <ue/nas/task.hpp>
 
-namespace nr::u
+namespace nr::ue
 {
 
 EProcRc NasMm::sendInitialRegistration(EInitialRegCause regCause)
@@ -1034,4 +1034,4 @@ void NasMm::resetRegAttemptCounter()
     m_storage->storedSuci->clear();
 }
 
-} // namespace nr::u
+} // namespace nr::ue
