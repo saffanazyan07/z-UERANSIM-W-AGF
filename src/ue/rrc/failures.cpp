1b2a99c2659916b27896319d76112734df9cfcf8
zyzy <131111603+saffanazyan07@users.noreply.github.com>
2024-10-16 16:36:10 +0800

    Update failures.cpp

--- src/ue/rrc/failures.cpp
+++ src/ue/rrc/failures.cpp
@@ -12,7 +12,7 @@
 #include <ue/nas/task.hpp>
 #include <ue/nts.hpp>
 
-namespace nr::u
+namespace nr::ue
 {
 
 void UeRrcTask::declareRadioLinkFailure(rls::ERlfCause cause)
@@ -26,4 +26,4 @@ void UeRrcTask::handleRadioLinkFailure(rls::ERlfCause cause)
     m_base->nasTask->push(std::make_unique<NmUeRrcToNas>(NmUeRrcToNas::RADIO_LINK_FAILURE));
 }
 
-} // namespace nr::u
+} // namespace nr::ue
