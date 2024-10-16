cc6b3650b1960e0077632ce038e172d938acea6d
zyzy <131111603+saffanazyan07@users.noreply.github.com>
2024-10-16 16:46:31 +0800

    Update service.cpp

--- src/ue/nas/mm/service.cpp
+++ src/ue/nas/mm/service.cpp
@@ -11,7 +11,7 @@
 #include <lib/nas/utils.hpp>
 #include <ue/nas/sm/sm.hpp>
 
-namespace nr::u
+namespace nr::ue
 {
 
 EProcRc NasMm::sendServiceRequest(EServiceReqCause reqCause)
@@ -487,4 +487,4 @@ void NasMm::receiveServiceReject(const nas::ServiceReject &msg)
     }
 }
 
-} // namespace nr::u
+} // namespace nr::ue
