772780847c1e9023c0e7b01836f92ff1bf2ed916
zyzy <131111603+saffanazyan07@users.noreply.github.com>
2024-10-16 16:35:28 +0800

    Update idle.cpp

--- src/ue/rrc/idle.cpp
+++ src/ue/rrc/idle.cpp
@@ -14,7 +14,7 @@
 #include <ue/nas/task.hpp>
 #include <ue/rls/task.hpp>
 
-namespace nr::u
+namespace nr::ue
 {
 
 void UeRrcTask::performCellSelection()
@@ -288,4 +288,4 @@ bool UeRrcTask::lookForAcceptableCell(ActiveCellInfo &cellInfo, CellSelectionRep
     return true;
 }
 
-} // namespace nr::u
+} // namespace nr::ue
