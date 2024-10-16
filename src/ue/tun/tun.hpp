afb30d761e7fde1e72f3111d9559235e769b3e10
zyzy <131111603+saffanazyan07@users.noreply.github.com>
2024-10-16 16:32:21 +0800

    Update tun.hpp

--- src/ue/tun/tun.hpp
+++ src/ue/tun/tun.hpp
@@ -10,10 +10,10 @@
 
 #include <string>
 
-namespace nr::u::tun
+namespace nr::ue::tun
 {
 
 int TunAllocate(const char *namePrefix, std::string &allocatedName, std::string &error);
 bool TunConfigure(const std::string &tunName, const std::string &ipAddress, int mtu, bool configureRouting, std::string &error);
 
-} // namespace nr::u::tun
+} // namespace nr::ue::tun
